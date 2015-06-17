f14b03c83cc0f477d2108b0592d98944af54bd01
pcpvector <pcpvector@gmail.com>
2015-06-17 12:07:42 +0530

    Implemented ServletContextListener

---
+++ src/main/java/com/rahul/Listenerdemo.java
@@ -0,0 +1,47 @@
+package com.rahul;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.measure.unit.SI;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.custardsource.parfait.Monitorable;
+import com.custardsource.parfait.MonitorableRegistry;
+import com.custardsource.parfait.MonitoredLongValue;
+import com.custardsource.parfait.dxm.IdentifierSourceSet;
+import com.custardsource.parfait.dxm.PcpMmvWriter;
+import com.custardsource.parfait.pcp.EmptyTextSource;
+import com.custardsource.parfait.pcp.MetricDescriptionTextSource;
+import com.custardsource.parfait.pcp.MetricNameMapper;
+import com.custardsource.parfait.pcp.PcpMonitorBridge;
+
+public class Listenerdemo implements ServletContextListener {
+	
+	 public static MonitoredLongValue done = 
+			  new  MonitoredLongValue(
+						 "visualiq.ums.app.sample",
+						 "Time spend indexing",
+						 MonitorableRegistry.DEFAULT_REGISTRY,
+						 
+						 1L, 
+						 SI.NANO(SI.SECOND)); 
+	 PcpMmvWriter bridge=new PcpMmvWriter("mmvname",IdentifierSourceSet.DEFAULT_SET);
+	 PcpMonitorBridge bridge1 = new PcpMonitorBridge(bridge, MetricNameMapper.PASSTHROUGH_MAPPER, new MetricDescriptionTextSource(), new EmptyTextSource());
+	 Collection<Monitorable<?>> coll = new ArrayList<Monitorable<?>>();
+	 
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		bridge1.stopMonitoring(coll);
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		 coll.add(done);
+		 bridge1.startMonitoring(coll);   
+		 
+	}
+	
+}
